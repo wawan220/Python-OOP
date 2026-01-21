bdddebaf4a2766eeaa16717d620552c9d1a9642a
wawan220 <mail@pohl-waldemar.info>
2026-01-21 13:27:01 +0100

    add lösung Bankkonto aufgabe

--- oop-herausforderung-aufgabe/bankonto.py
+++ oop-herausforderung-aufgabe/bankonto.py
@@ -19,9 +19,23 @@ class Bankkonto:
             print(f"leider kann der betrag {betrag:.2f}€ nicht abgebucht werden.")
             print("Kontostand zu niedrieg")
             print(f"aktueller Kontostand: >{self.kontostand:.2f}€")
+    
+    def __str__(self):
+        return f"der aktuelle kontostand beträgt:\n > {self.kontostand:.2f}€"
 
 
 
 konto1=Bankkonto(inhaber="waldi",kontostand=500)
+#print(f"{konto1.kontostand:.2f}")
+konto1.einzahlen(10.10)
+#print(f"{konto1.kontostand:.2f}")
+konto1.abbuchungen(60.10)
+#print(f"{konto1.kontostand:.2f}")
+konto1.abbuchungen(450)
+#print(f"{konto1.kontostand:.2f}")
+konto1.einzahlen(10)
+#print(f"{konto1.kontostand:.2f}")
+konto1.abbuchungen(20)
+#print(f"{konto1.kontostand:.2f}")
+print(konto1)
 
-print(f"{konto1.kontostand:.2f}")
